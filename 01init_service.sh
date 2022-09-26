6c9e9411411ee0194d26e4ba5045fe95012bc984
Mihail Struchalin <m.struchalin@gmail.com>
2022-09-26 19:58:09 +0300

    Add service timer

---
+++ 01init_service.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+cat >/etc/sysconfig/watchlog<<__EOF
+# Configuration file for my watchdog service
+# Place it to /etc/sysconfig
+# File and word in that file that we will be monit
+WORD="ALERT"
+LOG=/var/log/messages
+__EOF
+
+cat >/opt/watchlog.sh<<__EOF
+#!/bin/bash
+WORD=\$1
+LOG=\$2
+DATE=\`date\`
+if grep \$WORD \$LOG &> /dev/null
+then
+logger "\$DATE: I found word, Master!"
+else
+exit 0
+fi
+__EOF
+
+chmod +x /opt/watchlog.sh
+
+cat >/etc/systemd/system/watchlog.service<<__EOF
+[Unit]
+Description=My watchlog service
+[Service]
+Type=oneshot
+EnvironmentFile=/etc/sysconfig/watchlog
+ExecStart=/opt/watchlog.sh \$WORD \$LOG
+__EOF
+
+cat >/etc/systemd/system/watchlog.timer<<__EOF
+[Unit]
+Description=Run watchlog script every 30 second
+[Timer]
+# Run every 30 second
+OnActiveSec=1sec
+OnUnitActiveSec=30
+AccuracySec=1us
+Unit=watchlog.service
+[Install]
+WantedBy=multi-user.target
+__EOF
+
+systemctl daemon-reload && systemctl enable watchlog.timer && systemctl start watchlog.timer
+
+logger ALERT
--- Vagrantfile
+++ Vagrantfile
@@ -12,6 +12,8 @@ Vagrant.configure("2") do |config|
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
     end
 
+    server.vm.provision "shell", name: "Init timer service", path: "01init_service.sh"
+
   end
 
 end
